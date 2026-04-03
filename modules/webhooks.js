95abd05e8efad08c6dda19a198a388d938bce5e4
freepentests <252639873+freepentests@users.noreply.github.com>
2026-04-03 22:50:06 +0100

    added send, delete, and get methods to WebhookClient

--- modules/webhooks.js
+++ modules/webhooks.js
@@ -1,3 +1,33 @@
-class WebhookClient {
+import { DiscordApi } from './discordApi.js';
+
+export class WebhookClient {
+	constructor(data) {
+		if (data.url) {
+			this.url = data.url;
+		} else {
+			this.url = `https://discord.com/api/webhooks/${data.id}/${data.token}`;
+		}
+	}
+
+	send(data) {
+		return DiscordApi.fetch(this.url, {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: 'POST'
+		});
+	}
+
+	delete() {
+		return DiscordApi.fetch(this.url, {
+			method: 'DELETE'
+		});
+	}
+
+	get() {
+		return DiscordApi.fetch(this.url, {
+			method: 'GET'
+		});
+	}
 }
 
