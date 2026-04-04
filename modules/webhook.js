937a63ebd02b93a6447641c6860da74e4d565538
freepentests <252639873+freepentests@users.noreply.github.com>
2026-04-04 08:28:54 +0100

    added getMessage, editMessage, and deleteMessage to Webhook

--- modules/webhook.js
+++ modules/webhook.js
@@ -1,6 +1,6 @@
 import { DiscordApi } from './discordApi.js';
 
-export class WebhookClient {
+export class Webhook {
 	constructor(data) {
 		if (data.url) {
 			this.url = data.url;
@@ -27,6 +27,28 @@ export class WebhookClient {
 	get() {
 		return DiscordApi.fetch(this.url, {
 			method: 'GET'
+		});
+	}
+
+	getMessage(messageId) {
+		return DiscordApi.fetch(`${this.url}/messages/${messageId}`, {
+			method: 'GET'
+		});
+	}
+
+	deleteMessage(messageId) {
+		return DiscordApi.fetch(`${this.url}/messages/${messageId}`, {
+			method: 'DELETE'
+		});
+	}
+
+	editMessageId(messageId, newData) {
+		return DiscordApi.fetch(`${this.url}/messages/${messageId}`, {
+			headers: {
+				'Content-Type': 'appliation/json'
+			},
+			body: JSON.stringify(newData),
+			method: 'PATCH'
 		});
 	}
 }
